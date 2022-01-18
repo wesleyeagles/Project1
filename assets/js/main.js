858602cbb1f9fa1663693ecda69156523eee9fac
Wesley Carvalho <crafael.wesley@gmail.com>
2022-01-18 13:31:18 -0300

    Deply

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function aumentoSalarial() {
+
+var salario = document.getElementById("salario").value;
+var percentual = 0.10;
+var aumento = salario * percentual;
+var novo_salario = salario + aumento;
+
+novoSalario.innerHTML = Number(novo_salario);
+
+}
+
+
+
+
+
