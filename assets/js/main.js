04ba56c74a82e712450ad5cad9964fc214b2e87c
Wesley Carvalho <crafael.wesley@gmail.com>
2022-01-27 09:48:18 -0300

    deply

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,166 @@
-function aumentoSalarial() {
+var question1 = document.getElementById("question1")
+var question2 = document.getElementById("question2")
+var question3 = document.getElementById("question3")
+var question4 = document.getElementById("question4")
+var question5 = document.getElementById("question5")
+var question6 = document.getElementById("question6")
+var question7 = document.getElementById("question7")
+var question8 = document.getElementById("question8")
+var question9 = document.getElementById("question9")
+var question10 = document.getElementById("question10")
 
-var salario = document.getElementById("salario").value;
-var percentual = 0.10;
-var aumento = salario * percentual;
-var novo_salario = salario + aumento;
 
-novoSalario.innerHTML = Number(novo_salario);
+function changeOption() {
+
+    question1.classList.add("hidden")
+    question2.classList.remove("hidden")
+    question2.classList.add("none-class")
+
 
 }
 
+function changeOption2() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.remove("hidden")
+    question3.classList.add("none-class")
 
 
+}
+
+function changeOption3() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+
+    question4.classList.remove("hidden")
+    question4.classList.add("none-class")
 
 
+}
+
+function changeOption4() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+
+    question5.classList.remove("hidden")
+    question5.classList.add("none-class")
+
+
+}
+
+function changeOption5() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+
+    question6.classList.remove("hidden")
+    question6.classList.add("none-class")
+    
+
+
+}
+
+function changeOption6() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+
+    question7.classList.remove("hidden")
+    question7.classList.add("none-class")
+    
+
+
+}
+
+function changeOption7() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+    question7.classList.add("hidden")
+
+    question8.classList.remove("hidden")
+    question8.classList.add("none-class")
+    
+
+
+}
+
+function optionArquiteto() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+    question7.classList.add("hidden")
+    question8.classList.add("hidden")
+
+    question9.classList.remove("hidden")
+    question9.classList.add("none-class")
+}
+
+function optionPaisagista() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+    question7.classList.add("hidden")
+    question8.classList.add("hidden")
+
+    question9.classList.remove("hidden")
+    question9.classList.add("none-class")
+}
+
+function optionClienteFinal() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+    question7.classList.add("hidden")
+    question8.classList.add("hidden")
+
+    question9.classList.remove("hidden")
+    question9.classList.add("none-class")
+
+}
+
+function optionProjeto3d() {
+
+    question1.classList.add("hidden")
+    question2.classList.add("hidden")
+    question3.classList.add("hidden")
+    question4.classList.add("hidden")
+    question5.classList.add("hidden")
+    question6.classList.add("hidden")
+    question7.classList.add("hidden")
+    question8.classList.add("hidden")
+    question9.classList.add("hidden")
+
+    question10.classList.remove("hidden")
+    question10.classList.add("none-class")
+
+}
